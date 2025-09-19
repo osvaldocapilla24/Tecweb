b54ceeddf334ea2d07787fdb0abfa56806744d4a
osva <osvaldocapilla1@gmail.com>
2025-09-19 01:23:04 -0600

    se realizo el ejercicio 2

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -10,6 +10,26 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
    
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por:</p>
+    <form method="get">
+        <input type="hidden" name="ej2" value="1">
+        <input type="submit" value="Generar Secuencia">
+    </form>
+    <?php
+        if (isset($_GET['ej2'])) {
+            $matriz = generarSecuenciaImparParImpar();
+            echo "<table border='1'><tr><th>A</th><th>B</th><th>C</th></tr>";
+            foreach ($matriz as $fila) {
+                echo "<tr><td>{$fila[0]}</td><td>{$fila[1]}</td><td>{$fila[2]}</td></tr>";
+            }
+            echo "</table>";
+            echo "<p>Generados " . (count($matriz)*3) . " números en " . count($matriz) . " iteraciones.</p>";
+        }
+    ?>
+
+
 
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -11,5 +11,42 @@
             echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
         }
     }
+
+    function generarSecuenciaImparParImpar() 
+    {
+        $matriz = [];
+        do {
+            $a = rand(100, 999);
+            $b = rand(100, 999);
+            $c = rand(100, 999);
+            $matriz[] = [$a, $b, $c];
+        } while (!($a % 2 != 0 && $b % 2 == 0 && $c % 2 != 0));
+
+        return $matriz;
+    }
+
+    function primerMultiplo_while($n) 
+    {
+        $contador = 0;
+        while (true) {
+            $contador++;
+            $x = rand(1, 1000);
+            if ($x % $n == 0) {
+            return [$x, $contador];
+            }
+        }
+    }
+
+    // Ejercicio 3 con do-while
+    function primerMultiplo_dowhile($n) {
+    $contador = 0;
+        do {
+        $contador++;
+        $x = rand(1, 1000);
+        } while ($x % $n != 0);
+        return [$x, $contador];
+    }
+
+
     
     ?>
